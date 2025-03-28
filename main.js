214e703beff3984aea481308c3a0b2e51efce0c6
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-03-28 17:14:58 +0530

    commit

---
+++ main.js
@@ -0,0 +1,123 @@
+// VARIABLES 
+
+// var, let, const
+
+// var -> redeclare, update 
+// let -> not redeclare, update
+// const -> not redeclare, not update
+
+// variable_type variable_name = data;
+
+// var update 
+
+// var num1 = 10;
+// num1 = 30;
+// console.log(num1);
+
+// var redeclare 
+
+// var num2 = 10;
+// var num2 = 20;
+// console.log(num2)
+
+// let redeclare
+
+// let num = 5;
+// let num = 10;
+// console.log(num);
+
+// let update
+
+// let num = 5;
+// num = 10;
+// console.log(num);
+
+// const redeclare
+
+// const num = 5;
+// const num= 10;
+// console.log(num);
+
+// const num = 5;
+// num = 10;
+// console.log(num);
+
+// DATATYPES
+
+// NUMBER *,
+// STRING *,
+// BOOLEAN,
+// NULL,
+// UNDEFINED,
+// OBJECT *,
+// ARRAY *,
+// FUNCTION *,
+// DATE,
+
+// NUMBER 
+
+// var a = 10
+// console.log(a)
+// console.log(typeof(a))
+
+// STRING 
+
+// var a_b = '20'
+// console.log(a_b)
+// console.log(typeof(a_b))
+
+// BOOLEAN
+
+// var a = true
+// console.log(a)
+// console.log(typeof(a))
+
+// var a = false
+// console.log(a)
+// console.log(typeof(a))
+
+// var a = null;
+// console.log(a);
+// console.log(typeof(a))
+
+// var a;
+// console.log(a);
+
+// console.log(b);
+
+// {} object
+// [] array
+
+// OBJECT
+
+// SYNTAX 
+
+// variable_type object_name = {
+//     key : value,
+//     key : value,
+// }
+
+// var number = 3456789;
+// console.log(number);
+
+// var obj = {
+    // name : 'hello',
+    // place : 'trichy',
+    // number : 234567878
+// }
+// console.log(obj)
+// console.log(obj.name)
+// console.log(obj.place)
+// console.log(obj.number)
+// console.log(typeof(obj.name))
+
+// var details = {
+//     name : 'abi',
+//     age : 20,
+//     place : 'trichy'
+// }
+
+// console.log(details.place)
+// console.log(details.age)
+// console.log(details.name)
+// console.log(details)
