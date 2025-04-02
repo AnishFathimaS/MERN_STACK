c3c4276fed8a38462d943ecbc9f10f97e97a7545
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-04-02 18:45:45 +0530

    commit

--- main.js
+++ main.js
@@ -8,7 +8,37 @@
 
 // variable_type variable_name = data;
 
+// 3456789
+// 1
+// 1hello
+// hello-world
+// hello world
+
+// hello1
+// dfghj
+// a 
+// helloWorld
+// hello_world
+
 // var update 
+
+// var abc = 10; //declaration
+// var abc = 20; // re declare
+// var abc = 30; // re declare
+// console.log(abc)
+
+// var abc = 10; // declaration
+// console.log(abc); // 10
+// abc = 20; // update
+// console.log(abc); // 20
+
+// var abc = 10;
+// let abc = 20;
+// console.log(abc)
+
+// let abc = 10;
+// abc = 20;
+// console.log(abc);
 
 // var num1 = 10;
 // num1 = 30;
@@ -31,6 +61,14 @@
 // let num = 5;
 // num = 10;
 // console.log(num);
+
+// const abc = 10;
+// const abc = 20;
+// console.log(abc);
+
+// const abc = 10;
+// abc = 20;
+// console.log(abc);
 
 // const redeclare
 
@@ -56,31 +94,44 @@
 
 // NUMBER 
 
-// var a = 10
-// console.log(a)
+// var a = 10;
+// console.log(a);
 // console.log(typeof(a))
 
 // STRING 
+
+// var text = "Hello456789";
+// console.log(text)
+// console.log(typeof(text))
+
+// var abc =  hello;
+// console.log(abc)
+// console.log(typeof(abc))
 
 // var a_b = '20'
 // console.log(a_b)
 // console.log(typeof(a_b))
 
-// BOOLEAN
+// BOOLEAN - ( true, false )
 
 // var a = true
 // console.log(a)
 // console.log(typeof(a))
 
 // var a = false
-// console.log(a)
-// console.log(typeof(a))
+// console.log(a) // false
+// console.log(typeof(a)) // boolean
 
 // var a = null;
 // console.log(a);
 // console.log(typeof(a))
 
-// var a;
+// {}
+
+// not defined, 
+// undefined
+
+// var a ;
 // console.log(a);
 
 // console.log(b);
@@ -95,15 +146,27 @@
 // variable_type object_name = {
 //     key : value,
 //     key : value,
+//     key : value,
+//     key : value,
+//     key : value,
+//     key : value,
+//     key : value
 // }
+
+// var obj = {
+//     name : 'abinaya',
+//     age : 20,
+//     place : 'trichy'
+// }
+// console.log(obj.place)
 
 // var number = 3456789;
 // console.log(number);
 
 // var obj = {
-    // name : 'hello',
-    // place : 'trichy',
-    // number : 234567878
+//     name : 'hello',
+//     place : 'trichy',
+//     number : 234567878
 // }
 // console.log(obj)
 // console.log(obj.name)
@@ -121,3 +184,91 @@
 // console.log(details.age)
 // console.log(details.name)
 // console.log(details)
+
+// ARRAY 
+
+// SYNTAX 
+
+// variable_type array_name = [ data, data ];
+
+// let arr = [ 1, 2, 3, 'hello' ];
+// console.log(arr[3]);
+
+// let arr = ['abinaya', 20, 'trichy']
+// console.log(arr[0], arr[1])
+
+// index, position => starts with 0
+
+// function funciton_name(){
+    // code statement
+// }
+
+// function fun(){
+    // console.log('Hello')
+// }
+// fun()
+
+// sun = 0,
+// mon = 1,
+// tues = 2,
+// wed = 3
+
+// jan = 0, 
+// feb = 1, 
+// mar = 2,
+// april = 3
+
+// var todayDate = new Date();
+// console.log(todayDate);
+// console.log(todayDate.getDate());
+// console.log(todayDate.getDay());
+// console.log(todayDate.getMonth());
+// console.log(todayDate.getFullYear());
+// console.log(todayDate.getTime());
+// console.log(todayDate.get)
+
+// STRING CONCATENATION
+
+// const abc = 'Hello';
+// const text = 'World';
+// console.log(text, abc);
+// console.log(abc+" "+text);
+
+// var text = 'Hello'
+// console.log(text , 25, 'Javascript')
+
+// var text = 'Hello';
+// console.log(`${text} javascript`)
+
+// var text = 'javascript';
+// console.log(`Hello ${text} jasdfsdf`)
+
+// hello Javscript world
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
