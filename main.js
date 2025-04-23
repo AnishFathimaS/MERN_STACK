ef5d180692cc3dc7260f2fdec3dac0ed557e1cce
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-04-23 10:25:39 +0530

    commit

--- main.js
+++ main.js
@@ -1,203 +1,113 @@
-// ARITHMETIC OPERATOR +, -, *, **, /, %
+// var arr = ['html', 'css', 'bootstrap', 'javascript']
+// console.log(arr.push('abc'))
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
 
-// var a = 10;
-// var b = 5;
+// var arr = ['html', 'css', 'bootstrap', 'javascript']
+// console.log(arr.shift());
+// console.log(arr);
+// console.log(arr.unshift('abc'));
+// console.log(arr);
 
-// console.log(a+b);
+// var arr = ['html', 'css', 'bootstrap', 'javascript']
+// console.log(arr);
+// console.log(arr.join());
+// console.log(arr.join(''));
+// console.log(arr.join('--'));
+// console.log(arr.join('**'));
 
-// console.log(`${a} + ${b} = ${a+b}`);
-// console.log(`${a} - ${b} = ${a-b}`);
-// console.log(`${a} * ${b} = ${a*b}`);
-// console.log(`${a} / ${b} = ${a/b}`);
-// console.log(`${a} % ${b} = ${a%b}`);
-// console.log(`${a} ** ${b} = ${a**b}`);
+// var arr = ['html', 'css', 'bootstrap', 'javascript'];
+// console.log(arr);
+// console.log(arr.reverse());
 
-// ASSIGNMENT OPERATOR +=, -=, *=, /=
+// var arr = ['html', 'css', 'bootstrap', 'javascript'];
+// console.log(arr.sort());
 
-// var a = 2;
-// var b = 4;
+// var num = [30, 20, 100]
+// console.log(num.sort());
 
-// console.log(`a = ${a}, b = ${b}, a+=b = ${a+=b}`);
-// console.log(`a = ${a}, b = ${b}, a-=b = ${a-=b}`);
-// console.log(`a = ${a}, b = ${b}, a*=b = ${a*=b}`);
-// console.log(`a = ${a}, b=${b}, a/=b =${a/=b}`);
+// var num = [30, 20, 100]
+// console.log(num.sort((a, b) => a - b));
 
-// COMPARISON OPERATOR ==, !=, ===, !==
+// positive - swap , negative - no swap 
 
-// ==, data type not check, value check
+// a = 30, b = 20 => 30 - 20 = 10 [ 20, 30, 100 ]
+// a = 30, b = 100 => 30 - 100 = -70 [ 20, 30, 100 ]
 
-// var a = '8';
-// var b = 8;
+// var num = [30, 20, 100]
+// console.log(num.sort((a, b) => b - a));
 
-// console.log(a==b); // false
+// a = 30, b = 20 => 20 - 30 = -10 [ 30, 20, 100 ]
+// a = 20, b = 100 => 100 - 20 = 80 [ 30, 100, 20 ]
+// a = 30, b = 100 => 100 - 30 = 70 [ 100, 30, 20 ]
 
-// var a = 8;
-// var b = 8;
+// splice( position, delete-count, new-items )
 
-// console.log(a==b); // true
+// var a = ['html', 'css', 'bootstrap', 'javascript'];
 
-// var a = '4'
-// var b = 4
+// delete
 
-// console.log(a==b);
-
-
-// !=
-
-// var a = '5';
-// var b = 6;
-
-// console.log(a!=b);
-
-// === 
-
-// value, data type 
-
-// var a = '5';
-// var b = '5';
-
-// console.log(a === b);
-
-// var a = '5';
-// var b = 7;
-
-// value, data type 
-
-// console.log(a !== b);
-
-// function addFun(){
-//     var num1 = Number(document.getElementById('num1').value);
-//     var num2 = Number(document.getElementById('num2').value);
-//     document.getElementById('result').innerHTML = `Result = ${num1 + num2}`;
-// }
-
-// RELATIONAL OPERATOR <, >, <=, >=
-
-// var a = 10;
-// var b = 5;
-
-// console.log(a<=b); // 10 < 5
-
-// var a = 20;
-// var b = 10;
-// console.log(a<b); // false
-
-// var a = 5;
-// var b = 3;
-// console.log(a>b); // true
-
-// var a = 8;
-// var b = 5;
-// console.log(a>=b); // true
-
-// var a = 10;
-// var b = 20;
-// console.log(a<=b); // true
-
-// var a = 42;
-// var b = 42;
-// console.log(a<=b); // true
-
-// LOGICAL OPERATOR &&, ||, !
-
-// AND &&
-
-// console.log(true && true)
-// console.log(true && false)
-// console.log(false && true)
-// console.log(false && false)
-
-// console.log( 4<6 && 8>=8 ) // true && true = true
-// console.log( 6>3 && 7<3 ); // true && false = false
-// console.log( 9<4 && 5>3 ); // false && true = false
-// console.log( 8>20 && 10<6 ); // false && false = false
-
-// console.log(5<8 && 4>10 && 3>5); // true && false && false
-// console.log(8>5 && 5<10 && 8==8); // true && true && true
-
-// OR ||
-
-// console.log(true || true); // true
-// console.log(true || false); // true
-// console.log(false || true); // true
-// console.log(false || false); // false
-
-// console.log(10>6 || 6<8); // true || true => true
-// console.log(9>4 || 6>10); // true || false => true
-// console.log(7>8 || 6<10); // false || true => true
-// console.log(7<5 || 8>10); // false || false => false
-
-// console.log(5>3 || 6<10 || 7>10); // true || true || false
-// console.log(9<5 || 6!=6 || 8<5); // false || false || false
-
-// ! NOT 
-
-// console.log(!true) // false
-// console.log(!false) // true
-
-// console.log(!(8>5)); // false
-// console.log(!(5>10)); // true
-
-// console.log(!(8==='8' && 6<10)); // false && true => false
-
-// UNARY OPERATOR [ POST INCREMENT, PRE INCREMENT, POST DECREMENT, PRE DECREMENT ]
-
-// INCREMENT
-
-// POST
-
-// var a = 5;
-// console.log(a++); // a = a + 1
+// console.log(a.splice(2, 2));
 // console.log(a);
 
-// var a = 10;
-// console.log(a++); // 10
-// console.log(a++); // 11
-// console.log(a); // 12
+// delete & insert 
 
-// var a = 123;
-// console.log(a++); // 123
-// console.log(a); // 124
-// console.log(a++); // 124
-// console.log(a); // 125
+// console.log(a.splice(1, 1, 'abc'))
+// console.log(a)
 
-// PRE
+// insert
 
-// var a = 5;
-// console.log(++a); // a = a + 1 //6
-// console.log(a); // 6
+// console.log(a.splice(4, 0, 'abc'))
+// console.log(a)
 
-// var a = 6;
-// console.log(++a); // 7
-// console.log(++a); // 8
-// console.log(a); // 8
-// console.log(a++); // 8
-// console.log(a); // 9
+// slice ( position, length )
 
-// DECREMENT 
+// var a = ['html', 'css', 'bootstrap', 'javascript'];
+// console.log(a.slice(0, 2));
+// console.log(a.slice(2));
+// console.log(a.slice(0, 3));
+// console.log(a.slice(0, -2));
 
-// POST 
+// some
 
-// var a = 5;
-// console.log(a--); // a = a - 1 // 5
-// console.log(a); // 4
+// var a = [1, 2, 3, 5, 7];
+// console.log(a);
+// console.log(a.some((a) => a % 2 == 0));
 
-// console.log(a--); // 5
-// console.log(a); // 4
-// console.log(a); // 4
-// console.log(a--); // 4
-// console.log(a); // 3
+// var a = [1, 4, 6, 22, 7];
+// console.log(a)
+// console.log(a.some((a) => a < 0 ))
 
-// PRE
+// var obj = [
+//     {
+//         name : 'abinaya',
+//         age : 20
+//     },
+//     {
+//         name : 'anu',
+//         age : 35
+//     },
+//     {
+//         name : 'priya',
+//         age : 10
+//     },
+//     {
+//         name : 'hari',
+//         age : 27
+//     }
+// ]
 
-// var a = 4;
-// console.log(--a); // 3
-// console.log(a); // 3
+// console.log(obj);
+// console.log(obj.some((obj) => obj.age <= 18));
 
-// var a = 10;
-// console.log(--a); // 9
-// console.log(a--); // 9 ( 8 )
-// console.log(a); // 8
-// console.log(a--); // 8 ( 7 )
-// console.log(--a); // 7 - 1  = 6
-// console.log(a); // 6
+// every 
+
+// var a = [ 2, 4, 6, 8]
+// console.log(a.every((a) => a % 2 == 0))
+
+// var b = [ 100, 260, 350, 500 ];
+// console.log(b.every((b) => b <= 500))
+
+// var c = ['html', 'css', 'boostrap', 'javascript'];
+// console.log(c.every((c) => c.length > 3));
