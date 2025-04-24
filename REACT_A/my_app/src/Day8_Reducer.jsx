1a153f7c01d178213ebe82e242f213cd115eb39c
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-04-24 18:45:54 +0530

    commit

--- REACT_A/my_app/src/Day8_Reducer.jsx
+++ REACT_A/my_app/src/Day8_Reducer.jsx
@@ -46,52 +46,120 @@
 
 
 
-import React, { useReducer } from 'react';
+// import React, { useReducer } from 'react';
 
-const initialState = {
-  activeTab: 'home'
-};
+// const initialState = {
+//   activeTab: 'home'
+// };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'HOME':
-      return { activeTab: 'home' };
-    case 'ABOUT':
-      return { activeTab: 'about' };
-    case 'CONTACT':
-      return { activeTab: 'contact' };
-    default:
-      return state;
-  }
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'HOME':
+//       return { activeTab: 'home' };
+//     case 'ABOUT':
+//       return { activeTab: 'about' };
+//     case 'CONTACT':
+//       return { activeTab: 'contact' };
+//     default:
+//       return state;
+//   }
+// }
+
+// const TabSwitcher = () => {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+
+//   const renderContent = () => {
+//     switch (state.activeTab) {
+//       case 'home':
+//         return <p>🏠 Welcome to the Home Page!</p>;
+//       case 'about':
+//         return <p>ℹ️ This is the About Page.</p>;
+//       case 'contact':
+//         return <p>📞 Contact us at: example@email.com</p>;
+//       default:
+//         return null;
+//     }
+//   };
+
+//   return (
+//     <div style={{ textAlign: 'center' }}>
+//       <h2>Tab Switcher (useReducer)</h2>
+//       <div>
+//         <button onClick={() => dispatch({ type: 'HOME' })}>Home</button>
+//         <button onClick={() => dispatch({ type: 'ABOUT' })}>About</button>
+//         <button onClick={() => dispatch({ type: 'CONTACT' })}>Contact</button>
+//       </div>
+//       <div style={{ marginTop: '20px' }}>{renderContent()}</div>
+//     </div>
+//   );
+// };
+
+// export default TabSwitcher;
+
+// import React, { useState } from 'react'
+
+// const Day8_Reducer = () => {
+
+//     var [ count, setCount ] = useState(1)
+
+//     const addFun = () => {
+//         setCount(count + 1)
+//     }
+
+//     const subFun = () => {
+//         setCount(count - 1)
+//     }
+
+//     return (
+//         <>
+//             <button onClick={subFun}>-</button>
+//             <h1>{count}</h1>
+//             <button onClick={addFun}>+</button>
+//         </>
+//     )
+// }
+
+// export default Day8_Reducer
+
+// USE REDUCER 
+
+// SYNTAX
+
+// var [ state, dispatch ] = useReducer(function_name, object_value) 
+
+import React, { useReducer } from 'react'
+
+const Day8_Reducer = () => {
+
+    // const reducerFun = (state, action) => {
+    //     return 'hello'
+    // }
+
+    function reducerFun(){
+        return 'hello'
+    }
+
+    var [ state, dispatch ] = useReducer(reducerFun, { count : 1 })
+    console.log(dispatch());
+    // console.log(state)
+    // console.log(state.count)
+
+    const addFun = () => {
+        // console.log(dispatch);
+        // dispatch( {  } )
+    }
+
+    const subFun = () => {
+
+    }
+
+    return (
+        <>
+            <button onClick={subFun}>-</button>
+            <h1></h1>
+            <button onClick={addFun}>+</button>
+        </>
+    )
 }
 
-const TabSwitcher = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const renderContent = () => {
-    switch (state.activeTab) {
-      case 'home':
-        return <p>🏠 Welcome to the Home Page!</p>;
-      case 'about':
-        return <p>ℹ️ This is the About Page.</p>;
-      case 'contact':
-        return <p>📞 Contact us at: example@email.com</p>;
-      default:
-        return null;
-    }
-  };
-
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <h2>Tab Switcher (useReducer)</h2>
-      <div>
-        <button onClick={() => dispatch({ type: 'HOME' })}>Home</button>
-        <button onClick={() => dispatch({ type: 'ABOUT' })}>About</button>
-        <button onClick={() => dispatch({ type: 'CONTACT' })}>Contact</button>
-      </div>
-      <div style={{ marginTop: '20px' }}>{renderContent()}</div>
-    </div>
-  );
-};
-
-export default TabSwitcher;
+export default Day8_Reducer
--- main.js
+++ main.js
@@ -1,158 +1,157 @@
-// PUSH, POP 
+// FUNCTION
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript']
-// console.log(arr);
-// console.log(arr.push('abc'))
-// console.log(arr); // 
-// console.log(arr.pop()) // 
-// console.log(arr);
+// SYNTAX
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript']
-// console.log(arr.push('hello')) // 'html', 'css', 'bootstrap', 'javascript', 'hello'
-// console.log(arr.push('abc')) // 'html', 'css', 'bootstrap', 'javascript', 'hello', 'abc'
-// console.log(arr.pop()) // 'html', 'css', 'bootstrap', 'javascript', 'hello',
-// console.log(arr.push('abc')) // 'html', 'css', 'bootstrap', 'javascript', 'hello', 'abc'
-// console.log(arr) 
+// function function_name(){
+    // statement
+// }
+// function_name()
 
-// SHIFT, UNSHIFT
+// function fun(){
+//     console.log('hello')
+// }
+// fun()
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
-// console.log(arr.shift());
-// console.log(arr); 
-// console.log(arr.unshift('hello')); 
-// console.log(arr); 
+// function fun(){
+//     return 'hello'
+// }
+// console.log(fun()) 
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr.push('hello')); // 'html', 'css', 'bootstrap', 'javascript', 'hello
-// console.log(arr.push('java')); // 'html', 'css', 'bootstrap', 'javascript', 'hello', 'java'
-// console.log(arr.unshift('abc')); // abc, 'html', 'css', 'bootstrap', 'javascript', 'hello', 'java'
-// console.log(arr.shift()); //'html', 'css', 'bootstrap', 'javascript', 'hello', 'java'
-// console.log(arr.pop()); // 'html', 'css', 'bootstrap', 'javascript', 'hello', 
-// console.log(arr);
+// function fun(){
+//     return 'hello'
+// }
+// alert(fun())
 
-// JOIN
+// function fun(name){
+//     console.log(`My name is ${name}`)
+// }
+// fun('abinaya')
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
-// console.log(arr.join());
-// console.log(arr.join(''));
-// console.log(arr.join('  '));
-// console.log(arr.join('--'));
-// console.log(arr.join('***'));
+// my name is abinaya
+// my age is 20
 
-// REVERSE
+// function fun(age){
+//     alert(`My age is ${age}`)
+// }
+// fun(20)
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
-// console.log(arr.reverse());
+// function fun(name){
+//     return `my name is ${name}`
+// }
+// console.log(fun('abi'))
 
-// var num = [ 5, 4, 3, 2, 1 ];
-// console.log(num);
-// console.log(num.reverse());
+// function fun(name, age){
+    // console.log(`My name is ${name}`);
+    // console.log(`My age is ${age}`);
 
-// SORT
+    // console.log(`My name is ${name}, My age is ${age}`);
+// }
+// fun('abinaya', 20)
+// fun(20, 'abinaya')
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
-// console.log(arr.sort());
-// console.log(arr.sort().reverse());
+// function fun(name, age){
+//     return `My name is ${name} My age is ${age}`
+// }
+// console.log(fun('anu', 22));
 
-// var num = [ 30, 20, 100 ];
-// console.log(num);
-// console.log(num.sort()); // 100, 20, 30 
+// function fun(name = 'abinaya'){
+//     console.log(name)
+// }
+// fun()
 
-// console.log(num.sort((a,b) => a - b )) // 20, 30, 100
-// console.log(num.sort((a,b) => b - a )) // 100, 30, 20
+// function fun(name = 'abinaya', age = 20){
+    // console.log(name, age)
+// }
+// fun('anu', 25)
 
-// positive => swap
-// negative => no swap
+// function fun(name = 'anu', age='25'){
+//     return `${name} ${age}`
+// }
+// console.log(fun())
 
-// [ 30, 20, 100 ]
-// a = 30, b = 20, 30 - 20 = 10 [ 20, 30, 100 ]
-// a = 30, b = 100, 30 - 100 = -70 [ 20, 30, 100 ]
+// ARROW FUNCTION
 
-// [ 30, 20, 100 ]
-// a = 30, b = 20, 20 - 30 = -10 [ 30, 20, 100 ]
-// a = 20, b = 100, 100 - 20 = 80 [ 30, 100, 20 ]
-// a = 30, b = 100, 100 - 30 = 70 [ 100, 30, 20 ]
+// SYNTAX
 
-// SPLICE ( position, length, newitems )
+// variable_type function_name = () => {
+    // statement
+// }
+// function_name()
 
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
+// const fun = () => {
+//     console.log('hello');
+// }
+// fun()
 
-// remove
+// var fun = () => {
+//     return 'helloooooo'
+// }
+// console.log(fun());
 
-// console.log(arr.splice(0, 2));
-// console.log(arr);
-// console.log(arr.splice(2, 2));
-// console.log(arr);
+// let fun = (name) => {
+//     console.log(name)
+// }
+// fun('abi')
 
-// remove & insert 
+// const fun = (name, age) => {
+//     console.log(name , age);
+//     console.log(name + ' ' + age);
+// }
+// fun('abi', 20)
 
-// console.log(arr.splice(1, 1, 'hello'))
-// console.log(arr);
+// const fun = (name, email) => {
+//     return `My name is ${name}, My email is ${email}`
+// }
+// document.write(`<center><h1>${fun('abinaya', 'abinaya@email.com')}</h1></center>`)
 
-// console.log(arr.splice(2, 2, 'hello'))
-// console.log(arr)
+// const fun = (name = 'anu', address='trichy') => {
+//     console.log(name, address);
+// }
+// fun()
 
-// insert
 
-// console.log(arr.splice(2, 0, 'hello'));
-// console.log(arr);
+// JSON - javascript object notation
 
-// console.log(arr.splice(4, 0, 'react'));
-// console.log(arr);
+// SYNTAX
 
-// SLICE ( position, length )
-
-// var arr = ['html', 'css', 'bootstrap', 'javascript'];
-// console.log(arr);
-
-// console.log(arr.slice(0, 2)) // html, css
-// console.log(arr); // html, css, bootstrap, javascript
-
-// CONCAT
- 
-// var arr1 = ['html', 'css']
-// var arr2 = ['bootstrap', 'javascript']
-
-// console.log(arr1.concat(arr2));
-// console.log(arr2.concat(arr1));
-
-// var arr3 = ['hello', 'world']
-
-// console.log(arr1.concat(arr2, arr3))
-
-// console.log(arr1.concat('hello', 'world'))
-
-// SOME
-
-// var a = [ 1, 2, 3, 5, 7 ];
-// console.log(a.some((a) => a % 2 == 0 ));
-
-// var a = [ 1, 3, 5, -7, 4 ];
-// console.log(a.some((a) => a < 0 ))
-
-// EVERY
-
-// var a = [ 4, 2, 5, 8, 12 ];
-// console.log(a.every((a) => a % 2 == 0 ));
-
-// var a = [
+// var json = [
 //     {
 //         name : 'abi',
-//         age : 13
+//         age : 20,
+//         address : 'trichy'
 //     },
 //     {
 //         name : 'anu',
-//         age : 10
+//         age : 24,
+//         address : 'chennai'
 //     },
 //     {
 //         name : 'hari',
-//         age : 18
-//     },
+//         age : 26,
+//         address : 'salem'
+//     }
 // ]
 
-// console.log(a.every((a) => a.age <= 18 ))
+// console.log(json);
+// console.log(json[0]);
+// console.log(json[0].name);
+// console.log(json[2].address);
+
+// async await function 
+
+// async function fun(){
+//     var url = await fetch('https://jsonplaceholder.typicode.com/users')
+//     var data = await url.json();
+//     console.log(data[0].name);
+// }
+// fun()
+
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+let a = 30;
+let a = "high" // redeclare
+// a = 'high' // update
+console.log(a);
+
