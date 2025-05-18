b1d5f4772d4be80c149108b79a92035641f83012
AnishFathimaS <anisfathi0808@gmail.com>
2025-05-18 21:15:10 +0530

    commit

--- NODE JS/DAY_2/path.js
+++ NODE JS/DAY_2/path.js
@@ -1,4 +1,46 @@
-const path = require('path')
+// Example - 1
 
-const image = path.join('asset', 'images', 'img.jpg')
-console.log(image);
+// const path = require("path")
+
+// // const filepath = path.join("assets")
+// // const filepath = path.join("assets" , "images")
+// // const filepath = path.join("assets" , "images" , "img.jpg")
+// // const filepath = path.join(__dirname)
+// // const filepath = path.join(__dirname , "assets")
+// // const filepath = path.join(__dirname , "assets" , "images")
+// const filepath = path.join(__dirname , "assets" , "images" , "img.jpg")
+
+// console.log(filepath)
+
+
+// Example - 2
+
+// const path = require("path")
+
+// // const filepath = path.extname("example.html")
+// const filepath = path.extname("example.js")
+
+// console.log(filepath)
+
+
+// Example - 3
+
+// const http = require("http")
+// const file = require("fs")
+// const path = require("path")
+
+// const filepath = path.join(__dirname , "index.html")
+
+// file.writeFileSync(filepath , "Hello!")
+
+// const document = file.readFileSync(filepath , "utf8") 
+
+// const server = http.createServer((req , res) => {
+//     res.statusCode = 200;
+//     res.setHeader("content-type" , "text/html")
+//     res.end(document) 
+// })
+
+// server.listen(5000 , () => {
+//     console.log("Server Running Successfully!")
+// })
