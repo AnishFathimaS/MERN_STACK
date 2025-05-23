c6642fafc1261e0fb104f86b4dcd472377125b51
AnishFathimaS <anisfathi0808@gmail.com>
2025-05-23 07:04:18 +0530

    commit

--- REACT_CLASS/src/App.jsx
+++ REACT_CLASS/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import Day1_Components from './Day_1_Components'
-import Day_2_JS from './Day_2_JS'
-import Day_3_Style from './Day_3_Style'
-import Day_3_Props_1 from './Day_3_Props_1'
+// import Day1_Components from './Day_1_Components'
+// import Day_2_JS from './Day_2_JS'
+// import Day_3_Style from './Day_3_Style'
+// import Day_3_Props_1 from './Day_3_Props_1'
+import Day_4_State from './Day_4_State'
 
 const App = () => {
   return (
     <>
-      <Day1_Components />
-      <Day_2_JS />
-      <Day_3_Style />
-      <Day_3_Props_1 />
+      {/* <Day1_Components /> */}
+      {/* <Day_2_JS /> */}
+      {/* <Day_3_Style /> */}
+      {/* <Day_3_Props_1 /> */}
+      <Day_4_State />
     </>
   )
 }
