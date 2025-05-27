4d7b7c49e4d758e0bbe2746c0a82ab0ae9dcdf97
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-05-27 18:47:33 +0530

    commit

--- REACT_CLASS/src/Day_5_State_2.jsx
+++ REACT_CLASS/src/Day_5_State_2.jsx
@@ -19,7 +19,6 @@
 
 // export default Day_5_State_2
 
-    
 // import React, { useState } from 'react'
 
 // const Day_5_State_2 = () => {
@@ -28,23 +27,92 @@
 
 //     var [ qr, setQr ] = useState('')
 
-//     const inputText = (e) => {
+//     const fun = (e) => {
 //         setText(e.target.value)
 //     }
 
-//     const generateQR = () => {
-//         setQr(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`)
+//     const qrFun = () => {
+//         setQr(`https://api.qrserver.com/v1/create-qr-code/?size=180x180&data=${text}`)
 //         setText('')
 //     }
 
 //     return (
 //         <>
-//             <input type="text" value={text} onChange={inputText} />
-//             <button onClick={generateQR}>Generate</button>
+//             <input type="text" value={text} onChange={fun}/>
+//             <button onClick={qrFun}>Generate</button>
 //             <br />
-//             { qr ?  <img src={qr} alt="" /> : ''}
+//             { qr ? <img src={qr} alt="" /> : '' }
 //         </>
 //     )
 // }
 
 // export default Day_5_State_2
+
+
+    // import React, { useState } from 'react'
+    
+    // const Day_5_State_2 = () => {
+
+    //     var [ input, setInput ] = useState("")
+
+    //     const display = (e) => {
+    //         setInput(input + e.target.value);
+    //     }
+
+    //     const sum = () => {
+    //         try{
+    //             setInput(eval(input))
+    //         }
+    //         catch{
+    //             alert('Invalid Expression')
+    //             setInput('')
+    //         }
+    //     }
+
+    //     const allClear = () => {
+    //         setInput('')
+    //     }
+
+    //     const deleteNum = () => {
+    //         setInput(input.slice(0, -1))
+    //     }
+
+    //     return (
+    //         <>
+    //             <div>
+    //                 <input type="text" value={input} readOnly />
+    //             </div>
+    //             <div>
+    //                 <input type="button" value='1' onClick={display}/>
+    //                 <input type="button" value='2' onClick={display}/>
+    //                 <input type="button" value='3' onClick={display}/>
+    //                 <input type="button" value='4' onClick={display}/>
+    //             </div>
+    //             <div>
+    //                 <input type="button" value='5' onClick={display}/>
+    //                 <input type="button" value='6' onClick={display}/>
+    //                 <input type="button" value='7' onClick={display}/>
+    //                 <input type="button" value='8' onClick={display}/>
+    //             </div>
+    //             <div>
+    //                 <input type="button" value='9' onClick={display}/>
+    //                 <input type="button" value='0' onClick={display}/>
+    //                 <input type="button" value='00' onClick={display}/>
+    //                 <input type="button" value='.' onClick={display}/>
+    //             </div>
+    //             <div>
+    //                 <input type="button" value='+' onClick={display}/>
+    //                 <input type="button" value='-' onClick={display}/>
+    //                 <input type="button" value='*' onClick={display}/>
+    //                 <input type="button" value='/' onClick={display}/>
+    //             </div>
+    //             <div>
+    //                 <input type="button" value='=' onClick={sum}/>
+    //                 <input type="button" value='AC' onClick={allClear}/>
+    //                 <input type="button" value='DE' onClick={deleteNum}/>
+    //             </div>
+    //         </>
+    //     )
+    // }
+    
+    // export default Day_5_State_2
