eaebcba69675c112dfb48580a416278bc81871dc
ANISH FATHIMA S <anisfathi0808@gmail.com>
2025-05-28 18:49:11 +0530

    commit

--- REACT/my_app/src/App.jsx
+++ REACT/my_app/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Swiper_Package from './Swiper_Package'
+// import Swiper_Package from './Swiper_Package'
 // import Asset from './Asset.jsx'
 // import Day1_Components from './Day1_Components.jsx'
 // import Day2_JS from './Day2_JS.jsx'
 // import Day3_Props_1 from './Day3_Props_1.jsx'
-// import Day4_State_1 from './Day4_State_1.jsx'
+import Day4_State_1 from './Day4_State_1.jsx'
 // import Day5_State_2 from './Day5_State_2.jsx'
 // import Bootstrap from './Bootstrap.jsx'
 // import Day6_Effect_1 from './Day6_Effect_1.jsx'
@@ -34,7 +34,7 @@ const App = () => {
 
       {/* <Day3_Props_1 /> */}
 
-      {/* <Day4_State_1 /> */}
+      <Day4_State_1 />
 
       {/* <Day5_State_2 /> */}
 
